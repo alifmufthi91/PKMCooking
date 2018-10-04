0c551c95a296accbaeab79c01db09fcd9d574d9e
Kienz <alif.stinky@gmail.com>
2018-10-04 21:44:53 +0700

    new updated project

---
+++ app/src/main/java/com/example/kienz/cooqueen/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.kienz.cooqueen;
+
+
+
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+
+public class MainActivity extends AppCompatActivity
+        implements tab1.OnFragmentInteractionListener,tab2.OnFragmentInteractionListener,tab3.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(myPagerAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
+    }
+
+
+}
---
+++ app/src/main/java/com/example/kienz/cooqueen/MyPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.kienz.cooqueen;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class MyPagerAdapter extends FragmentStatePagerAdapter  {
+    public MyPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new tab1();
+            case 1: return new tab2();
+            case 2: return new tab3();
+        }
+        return null;
+    }
+
+    @Override
+
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0: return "Tab 1";
+            case 1: return "Tab 2";
+            case 2: return "Tab 3";
+            default: return null;
+        }
+    }
+}

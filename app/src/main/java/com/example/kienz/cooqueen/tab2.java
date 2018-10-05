ecaa0d82fc177163d4312628dcf15d73e0bd22c9
Kienz <alif.stinky@gmail.com>
2018-10-05 21:52:48 +0700

    enak gini warnanya, sama susah kalo putih cari theme nya

--- app/src/main/java/com/example/kienz/cooqueen/tab2.java
+++ app/src/main/java/com/example/kienz/cooqueen/tab2.java
@@ -1,12 +1,15 @@
 package com.example.kienz.cooqueen;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -17,7 +20,8 @@ import android.view.ViewGroup;
  * Use the {@link tab2#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class tab2 extends Fragment {
+public class tab2 extends Fragment  {
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,6 +62,8 @@ public class tab2 extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
@@ -65,6 +71,19 @@ public class tab2 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab2, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Button btn1 = (Button) view.findViewById(R.id.changeac);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), RecipeDetail.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
     // TODO: Rename method, update argument and hook method into UI event

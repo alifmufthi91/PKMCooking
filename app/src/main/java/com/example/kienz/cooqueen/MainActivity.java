ef9d6e02d75c3bb29bf200d685f763b4e956ed17
Kienz <alif.stinky@gmail.com>
2018-10-07 10:12:34 +0700

    ganti toolbar + tambah setting menu

--- app/src/main/java/com/example/kienz/cooqueen/MainActivity.java
+++ app/src/main/java/com/example/kienz/cooqueen/MainActivity.java
@@ -9,6 +9,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.net.Uri;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,7 +23,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(myPagerAdapter);
@@ -34,5 +38,21 @@ public class MainActivity extends AppCompatActivity
         //you can leave it empty
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // Action goes here
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
